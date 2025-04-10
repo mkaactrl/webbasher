bdecc31a027856720e93553d8f6eb46aebe409bb
emkot <flyerccount41@gmail.com>
2025-04-10 03:18:22 -0400

    Create weed.js

---
+++ other trolls/weed.js
@@ -0,0 +1,3 @@
+// Hotkey message that sends users straight to Websim with a single "weed"
+document.body.innerHTML = "<h1>WEED</h1>";
+window.location = "https://websim.ai"; // Sends 'em straight to Websim
